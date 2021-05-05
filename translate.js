89f229e2b4e5138fc705a78f4ed3e9dbc7896a1a
Rémi ALBAN <59766320+remialban@users.noreply.github.com>
2021-05-05 09:16:04 +0200

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,23 +1,36 @@
-function Translate(languages)
+function Translate(languages, defaultLanguage)
 {
 
-    var elements=document.querySelectorAll("[data-translate]")
-
-    console.log(navigator.language)
-    
-    
-    var translations = {}
+    var elements=document.querySelectorAll("[data-translate]")  
+    var translations = null
+    var defaultTranslations = null
     languages.map((item) => {
         item.lang.map((lang) => {
             if(lang == navigator.language)
             {
                 translations = item.trans
             }
+            if(lang == defaultLanguage)
+            {
+                defaultTranslations = item.trans
+            }
         })
         
     })
     
+    if(translations == null)
+    {
+        translations = defaultTranslations
+    }
     elements.forEach((e) => {
-        e.textContent = translations[e.dataset.translate]
+        if(translations[e.dataset.translate]== undefined)
+        {
+            e.innerHTML = defaultTranslations[e.dataset.translate]
+        } else 
+        {
+            e.innerHTML = translations[e.dataset.translate]
+        }
     })
+
+    document.querySelector("html").setAttribute("lang", navigator.language.substr(0,2))
 }
